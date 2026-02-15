e37bbce902425511e7c67e451ec0297d620dc8a5
jedcrisp <59922178+jedcrisp@users.noreply.github.com>
2026-02-15 10:26:28 -0600

    Fix Cross-Origin-Opener-Policy to allow Firebase Auth popups

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups', // Allows OAuth popups to work
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
